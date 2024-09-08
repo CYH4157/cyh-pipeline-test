5e530d0ac5af097315999d1bbc2bcb76236d71a0
CYH4157 <47914438+CYH4157@users.noreply.github.com>
2024-09-09 00:25:51 +0800

    Update llamaindex_testcode.py

--- llamaindex_testcode.py
+++ llamaindex_testcode.py
@@ -1,5 +1,4 @@
 from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
-from llama_index.embeddings.huggingface import HuggingFaceEmbedding
 from llama_index.llms.ollama import Ollama
 
 documents = SimpleDirectoryReader("data").load_data()
