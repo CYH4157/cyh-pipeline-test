ec4d5dd2d1df51c5cb9ffe620792252a519479df
CYH4157 <47914438+CYH4157@users.noreply.github.com>
2024-09-09 00:36:47 +0800

    Update llamaindex_testcode.py

--- llamaindex_testcode.py
+++ llamaindex_testcode.py
@@ -2,15 +2,17 @@ from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
 from llama_index.llms.ollama import Ollama
 from llama_index.embeddings.ollama import OllamaEmbedding
 documents = SimpleDirectoryReader("data").load_data()
-
+print(f'documents:{documents}')
 # bge-base embedding model
 Settings.embed_model = OllamaEmbedding(model_name="nomic-embed-text", base_url="http://172.17.0.1:11434")
 
 # ollama
 Settings.llm = Ollama(model="llama3", base_url="http://172.17.0.1:11434", request_timeout=360.0)
 
+print(f'======== VectorStoreIndex ==========')
 index = VectorStoreIndex.from_documents(
     documents,
+    show_progress=True
 )
 
 
